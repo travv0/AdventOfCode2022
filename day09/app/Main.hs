c164fe4c9417044e9dd476ea54ea717e9c9a40b2
Travis <tssund93@gmail.com>
2022-12-22 11:47:28 -0600

    day 9 part 2

--- day09/app/Main.hs
+++ day09/app/Main.hs
@@ -6,29 +6,34 @@ import qualified Data.Set as Set
 
 data Direction = R | L | U | D deriving (Show, Read, Eq)
 
-data Rope = Rope (Int, Int) (Int, Int) deriving (Show)
+type Rope = [(Int, Int)]
 
 touching :: (Int, Int) -> (Int, Int) -> Bool
 touching (x1, y1) (x2, y2) = abs (x1 - x2) < 2 && abs (y1 - y2) < 2
 
-step :: Direction -> Rope -> Rope
-step U (Rope (hx, hy) tl) = Rope newHead $ stepTail (Rope newHead tl)
-  where
-    newHead = (hx, hy - 1)
-step D (Rope (hx, hy) tl) = Rope newHead $ stepTail (Rope newHead tl)
-  where
-    newHead = (hx, hy + 1)
-step L (Rope (hx, hy) tl) = Rope newHead $ stepTail (Rope newHead tl)
-  where
-    newHead = (hx - 1, hy)
-step R (Rope (hx, hy) tl) = Rope newHead $ stepTail (Rope newHead tl)
-  where
-    newHead = (hx + 1, hy)
+stepHead :: Direction -> (Int, Int) -> (Int, Int)
+stepHead dir (hx, hy) = case dir of
+  U -> (hx, hy - 1)
+  D -> (hx, hy + 1)
+  L -> (hx - 1, hy)
+  R -> (hx + 1, hy)
 
-stepTail :: Rope -> (Int, Int)
-stepTail (Rope h@(hx, hy) t@(tx, ty))
-  | touching h t = t
+step :: Direction -> Rope -> Rope
+step _ [] = error "out of rope"
+step dir [h] = [stepHead dir h]
+step dir (h : t) = newHead : stepTail newHead t
+  where
+    newHead = stepHead dir h
+
+stepTail :: (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+stepTail _ [] = []
+stepTail h@(hx, hy) tl@(t@(tx, ty) : ts)
+  | touching h t = tl
   | otherwise =
+      newHead
+        : stepTail newHead ts
+  where
+    newHead =
       ( if hx > tx then tx + 1 else if hx < tx then tx - 1 else hx,
         if hy > ty then ty + 1 else if hy < ty then ty - 1 else hy
       )
@@ -36,18 +41,23 @@ stepTail (Rope h@(hx, hy) t@(tx, ty))
 stepTimes :: Int -> Direction -> Set (Int, Int) -> Rope -> (Rope, Set (Int, Int))
 stepTimes 0 _ ts rope = (rope, ts)
 stepTimes n dir ts rope =
-  let newRope@(Rope _ t) = step dir rope
+  let newRope = step dir rope
+      t = last newRope
    in stepTimes (n - 1) dir (Set.insert t ts) newRope
 
 parse :: String -> [(Direction, Int)]
 parse = map (\l -> (read $ take 1 l, read $ drop 2 l)) . lines
 
+doMotions :: Foldable t => Int -> t (Direction, Int) -> (Rope, Set (Int, Int))
+doMotions len =
+  foldl'
+    (\(rope, t) (dir, n) -> stepTimes n dir t rope)
+    (replicate len (0, 0), Set.empty)
+
 main :: IO ()
 main = do
   motions <- parse <$> readFile "input.txt"
-  let (_, ts) =
-        foldl'
-          (\(rope, t) (dir, n) -> stepTimes n dir t rope)
-          (Rope (0, 0) (0, 0), Set.empty)
-          motions
-  print $ Set.size ts
+  let (_, ts2) = doMotions 2 motions
+  print $ Set.size ts2
+  let (_, ts10) = doMotions 10 motions
+  print $ Set.size ts10
